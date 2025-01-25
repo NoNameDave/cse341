e0606365c1b3fd152f84bb9affdc08d61eaffd77
NoNameDave <chis.wol@gmail.com>
2025-01-25 16:38:09 +0200

    update swagger and add try/catch

--- Bookstore_API/controllers/books.js
+++ Bookstore_API/controllers/books.js
@@ -1,123 +1,125 @@
 const mongodb = require("../data/database");
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('books').find();
-    result.toArray().then((books) => {
-        res.setHeader('Content-Type', 'application/json');
+    try {
+        const result = await mongodb.getDatabase().db().collection("books").find();
+        const books = await result.toArray();
+        res.setHeader("Content-Type", "application/json");
         res.status(200).json(books);
-    });
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while fetching books.", error: error.message });
+    }
 };
 
 const getSingle = async (req, res) => {
-    const bookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('books').find({_id: bookId});
-    result.toArray().then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books[0]);
-    });
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection("books").findOne({ _id: bookId });
+
+        if (!result) {
+            return res.status(404).json({ message: "Book not found." });
+        }
+
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while fetching the book.", error: error.message });
+    }
 };
 
 const createBook = async (req, res) => {
-    //#swagger.tags = ['Books']
-    //#swagger.description = 'Create a new book'
-    //#swagger.parameters['body'] = {
-    //#swagger.in = 'body',
-    //#swagger.required = true,
-    //#swagger.schema = {
-    //  "title": "string",
-    //  "author": "string",
-    //  "genre": "string",
-    //  "price": "number",
-    //  "ISBN": "string",
-    //  "stock": "number",
-    //  "description": "string",
-    //  "imageUrl": "string"
-    // },
-    //#swagger.required = ["title", "author", "genre", "price", "ISBN", "stock"]
-    //}
+    try {
+        const { title, author, genre, price, ISBN, stock } = req.body;
 
-    const { title, author, genre, price, ISBN, stock } = req.body;
+        // List of required fields
+        const requiredFields = { title, author, genre, price, ISBN, stock };
 
-    // List of required fields
-    const requiredFields = { title, author, genre, price, ISBN, stock };
+        // Identify missing fields
+        const missingFields = Object.keys(requiredFields).filter(field => !requiredFields[field]);
 
-    // Identify missing fields
-    const missingFields = Object.keys(requiredFields).filter(field => !requiredFields[field]);
+        if (missingFields.length > 0) {
+            return res.status(400).json({
+                message: `The following required fields are missing: ${missingFields.join(", ")}`,
+            });
+        }
 
-    // If there are missing fields, return a specific error message
-    if (missingFields.length > 0) {
-        return res.status(400).json({ 
-            message: `The following required fields are missing: ${missingFields.join(', ')}` 
-        });
-    }
+        // Proceed with creating the book
+        const book = {
+            title,
+            author,
+            genre,
+            price,
+            ISBN,
+            stock,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+        };
 
-    // Proceed with creating the book
-    const book = { title, author, genre, price, ISBN, stock, description: req.body.description, imageUrl: req.body.imageUrl };
-    const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
-    if (response.acknowledged) {
-        res.status(201).send();
-    } else {
-        res.status(500).json(response.error || "Some error occurred while creating the book");
+        const response = await mongodb.getDatabase().db().collection("books").insertOne(book);
+
+        if (response.acknowledged) {
+            res.status(201).json({ message: "Book created successfully!" });
+        } else {
+            res.status(500).json({ message: "Failed to create the book." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while creating the book.", error: error.message });
     }
 };
 
-
 const updateBook = async (req, res) => {
-    //#swagger.tags = ['Books']
-    //#swagger.description = 'Update an existing book'
-    //#swagger.parameters['id'] = { in: 'path', required: true, description: 'Book ID' }
-    //#swagger.parameters['body'] = {
-    //#swagger.in = 'body',
-    //#swagger.required = true,
-    //#swagger.schema = {
-    //  "title": "string",
-    //  "author": "string",
-    //  "genre": "string",
-    //  "price": "number",
-    //  "ISBN": "string",
-    //  "stock": "number",
-    //  "description": "string",
-    //  "imageUrl": "string"
-    // },
-    //#swagger.required = ["title", "author", "genre", "price", "ISBN", "stock"]
-    //}
-    const bookId = new ObjectId(req.params.id);
-    
-    const { title, author, genre, price, ISBN, stock } = req.body;
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const { title, author, genre, price, ISBN, stock } = req.body;
 
-    // List of required fields
-    const requiredFields = { title, author, genre, price, ISBN, stock };
+        // List of required fields
+        const requiredFields = { title, author, genre, price, ISBN, stock };
 
-    // Identify missing fields
-    const missingFields = Object.keys(requiredFields).filter(field => !requiredFields[field]);
+        // Identify missing fields
+        const missingFields = Object.keys(requiredFields).filter(field => !requiredFields[field]);
 
-    // If there are missing fields, return a specific error message
-    if (missingFields.length > 0) {
-        return res.status(400).json({ 
-            message: `The following required fields are missing: ${missingFields.join(', ')}` 
-        });
-    }
+        if (missingFields.length > 0) {
+            return res.status(400).json({
+                message: `The following required fields are missing: ${missingFields.join(", ")}`,
+            });
+        }
 
-    // Proceed with creating the book
-    const book = { title, author, genre, price, ISBN, stock, description: req.body.description, imageUrl: req.body.imageUrl };
+        const book = {
+            title,
+            author,
+            genre,
+            price,
+            ISBN,
+            stock,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+        };
 
-    const response = await mongodb.getDatabase().db().collection('books').replaceOne({_id: bookId}, book);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the book");
+        const response = await mongodb.getDatabase().db().collection("books").replaceOne({ _id: bookId }, book);
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: "Book updated successfully!" });
+        } else {
+            res.status(404).json({ message: "Book not found or no changes made." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while updating the book.", error: error.message });
     }
 };
 
 const deleteBook = async (req, res) => {
-    //#swagger.tags = ['Books']
-    const bookId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('books').deleteOne({_id: bookId}, true);
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the book");
+    try {
+        const bookId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection("books").deleteOne({ _id: bookId });
+
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: "Book deleted successfully!" });
+        } else {
+            res.status(404).json({ message: "Book not found." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while deleting the book.", error: error.message });
     }
 };
 
@@ -126,5 +128,5 @@ module.exports = {
     getSingle,
     createBook,
     updateBook,
-    deleteBook
+    deleteBook,
 };
