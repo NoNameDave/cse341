5abc25c2b71cf1377664bb80574bf7377fbc656c
NoNameDave <chis.wol@gmail.com>
2025-02-02 13:18:36 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,37 +2,67 @@ const mongodb = require("../data/database");
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
+   try {
+           const result = await mongodb.getDatabase().db().collection("users").find();
+           const books = await result.toArray();
+           res.setHeader("Content-Type", "application/json");
+           res.status(200).json(books);
+       } catch (error) {
+           res.status(500).json({ message: "An error occurred while fetching users.", error: error.message });
+       }
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
-    });
+    try {
+            const userId = new ObjectId(req.params.id);
+            const result = await mongodb.getDatabase().db().collection("users").findOne({ _id: userId });
+    
+            if (!result) {
+                return res.status(404).json({ message: "User not found." });
+            }
+    
+            res.setHeader("Content-Type", "application/json");
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred while fetching the user.", error: error.message });
+        }
 };
 
 const createUser = async (req, res) => {
-    //#swagger.tags = ['Users']
-    const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-    }
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while creating the user");
-    }
+    try {
+            const { firstName, lastName, email} = req.body;
+    
+            // List of required fields
+            const requiredFields = { firstName, lastName, email};
+    
+            // Identify missing fields
+            const missingFields = Object.keys(requiredFields).filter(field => !requiredFields[field]);
+    
+            if (missingFields.length > 0) {
+                return res.status(400).json({
+                    message: `The following required fields are missing: ${missingFields.join(", ")}`,
+                });
+            }
+    
+            // Proceed with creating the book
+            const user = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                favoriteColor: req.body.favoriteColor,
+                birthday: req.body.birthday
+            };
+    
+            const response = await mongodb.getDatabase().db().collection("users").insertOne(user);
+    
+            if (response.acknowledged) {
+                res.status(201).json({ message: "User created successfully!" });
+            } else {
+                res.status(500).json({ message: "Failed to create the user." });
+            }
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred while creating the user.", error: error.message });
+        }
 };
 
 const updateUser = async (req, res) => {
